3c8e74cdac7aba472b6eb8abb4af8b6f7c0a18f3
Rob Carnell <bertcarnell@gmail.com>
2018-12-24 13:40:19 -0500

    Added spline functionality for smoothing

--- ForceIntegration.m
+++ ForceIntegration.m
@@ -43,6 +43,7 @@ position_b = 90.0;
 zero_force = -0.0610;
 zero_force_trim = -0.0615;
 max_force = min(force);
+# find all points where position is between position_a and position_b and force is less that the zero line
 idx = position > position_a  & position < position_b & force < zero_force_trim;
 position_clipped = position(idx);
 force_clipped = force(idx);
@@ -55,4 +56,19 @@ set(gca, "fontsize", 24);
 
 work = trapz(position_clipped, force_clipped - zero_force);
 work
+
+# splinefitting 
+# breaks is the places where the splines are fit together
+# linpace fills the space with a certain number of points
+breaks = linspace(position_a, position_b, 100);
+spline_force = splinefit(position_clipped, force_clipped, breaks, "order", 2, "periodic", true);
+
+figure(1, "position", [1,1,1800,1200]);
+plot(position_clipped, force_clipped, "ob", 
+     position_clipped, ppval(spline_force, position_clipped), "-r");
+xlabel("Position (mm)");
+ylabel("Force (N)");
+set(gca, "fontsize", 24);
+
+
 
